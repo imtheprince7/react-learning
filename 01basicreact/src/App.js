ee2533a8d73ecdf123585f06a2e2b82ceefc1c64
imtheprince7 <princeks3577@gmail.com>
2024-09-22 11:08:12 +0530

    01-basic

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+
+function App() {
+  <h1>  Hello World | ReactJs </h1>
+
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  <>
+  
+  <h1>Hello World | Vite</h1>
+  
+  </>
+}
+
+export default App
